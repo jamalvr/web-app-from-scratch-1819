713c13bff996c8909fbc45c352e33243121a7dbf
Jamal van Rooijen <jamal@vergelijkgroep.nl>
2019-07-29 22:59:00 +0200

    Cleaned up fetch .then

--- script.js
+++ script.js
@@ -10,24 +10,24 @@ const app = {
 let pokemonData = [];
 
 // Request
-const getPokemonData = function () {
+getPokemonData = function () {
     fetch('https://pokeapi.co/api/v2/pokedex/1/')
-        .then(
-            function (response) {
-                if (response.status !== 200) {
-                    console.log('Looks like there was a problem. Status Code: ' +
-                        response.status);
-                    return;
-                }
-
-                // Examine the text in the response
-                response.json().then(function (pokemon) {
-                    pokemonData.push(pokemon);
-                    // return (pokemon);
-                });
+        .then(function (response) {
+            // Log status if you get a response that's not working
+            if (response.status !== 200) {
+                console.log('Woops, no pokemon for you. Status Code: ' + response.status);
+                return;
             }
-        )
+
+            // Format response to JSON
+            return response.json();
+        })
+        .then(function (response) {
+            // Push data to global variable
+            pokemonData.push(response);
+        })
         .catch(function (error) {
+            // Error handling in case of error
             console.log('No pokemon because of:', error);
         });
 }
