a69acd187d87906619adfbefd2dad3b8f339b138
Jamal van Rooijen <jamal@vergelijkgroep.nl>
2019-07-30 09:56:50 +0200

    Fetch loop for pokemon specificx

--- script.js
+++ script.js
@@ -10,26 +10,48 @@ const app = {
 let pokemonData = [];
 
 // Request
-getPokemonData = function () {
-    fetch('https://pokeapi.co/api/v2/pokedex/1/')
-        .then(function (response) {
-            // Log status if you get a response that's not working
-            if (response.status !== 200) {
-                console.log('Woops, no pokemon for you. Status Code: ' + response.status);
-                return;
-            }
+const getPokemonData = function () {
+    for (let pokemonIndex = 1; pokemonIndex <= 5; pokemonIndex++) {
+        fetch('https://pokeapi.co/api/v2/pokemon-species/' + pokemonIndex)
+            .then(function (response, reject) {
+                // Log status if you get a response that's not working
+                if (response.status !== 200) {
+                    console.log('Woops, there is a problem with your pokemon. Status Code: ' + response.status);
+                    return;
+                }
 
-            // Format response to JSON
-            return response.json();
-        })
-        .then(function (response) {
-            // Push data to global variable
-            pokemonData.push(response);
-        })
-        .catch(function (error) {
-            // Error handling in case of error
-            console.log('No pokemon because of:', error);
-        });
+                // Format response to JSON
+                return response.json();
+            })
+            .then(function (response) {
+                console.log(response);
+            })
+             .catch(function (error) {
+                 // Error handling in case of error
+                 console.log('No pokemon because of:', error);
+             });
+    }
+    // .then(function (response) {
+
+    //     let pokemonDetailsUrl = response.pokemon_entries.map(function (pokemon_entry) {
+    //         return pokemon_entry.pokemon_species.url;
+    //     });
+    //     return pokemonDetailsUrl;
+    // })
+    // .then(function (pokemonDetailsUrl) {
+    //     console.log(pokemonDetailsUrl);
+
+    //     // Push data to global variable
+    //     pokemonData.push(pokemonDetailsUrl);
+    // })
+    // .catch(function (error) {
+    //     // Error handling in case of error
+    //     console.log('No pokemon because of:', error);
+    // });
+}
+
+const mapData = function () {
+
 }
 
 app.init();
