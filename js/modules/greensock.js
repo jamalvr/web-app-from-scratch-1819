f0fb84be5d55eedfa8d52afa5d775b97740d8a4e
Jamal van Rooijen <jamal@vergelijkgroep.nl>
2019-10-15 10:54:07 +0200

    Object literals p1

--- js/modules/greensock.js
+++ js/modules/greensock.js
@@ -1,9 +1,37 @@
-const greenSock = function () {
-    let card = querySelector('.pokemon-card');
+//// SVG animation
+const eaLoader = function () {
+    if (document.getElementById('loader-container')) {
+        // Individual elements
+        let cadeau = document.getElementById('loader-cadeau');
+        let controller = document.getElementById('loader-controller');
+        let korting = document.getElementById('loader-korting');
+        let cashback = document.getElementById('loader-cashback');
 
-    TweenLite(card, 1, {
-        y: -100
-    }, {
-        y: 0
-    })
+        // Grouped elements
+        let product = document.getElementById('loader-product');
+        let besparen = document.getElementById('loader-besparen');
+
+        const tl = new TimelineMax({
+            repeat: -1,
+            yoyo: true
+        });
+
+        tl.to(product, 1, {
+                css: {
+                    left: '100px'
+                }
+            })
+            .set(besparen, {
+                css: {
+                    zIndex: 1
+                }
+            }) // set is basically a 0-second duration tween
+            .to(besparen, 1, {
+                css: {
+                    left: '100px'
+                }
+            });
+    }
 }
+
+eaLoader();
