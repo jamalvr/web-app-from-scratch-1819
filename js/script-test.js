cace8d9321cfe87e51ccab2c3f50a96c49cddec9
Jamal van Rooijen <jamal@vergelijkgroep.nl>
2019-10-21 11:28:46 +0200

    Code split in modules

--- js/script-test.js
+++ js/script-test.js
@@ -2,17 +2,12 @@
 
 import data from './modules/data.js';
 import toggleMenu from './modules/toggleMenu.js';
-import router from './modules/router.js';
+import router from './router.js';
 
 (function () {
-
-    //// Set global var
-    let globalPokemon = [];
-    let statusFilter;
-    let statusSort;
-
     //// App structure
     const app = function () {
+        console.log(data);
         data.ready()
             .then(function () {
                 router();
